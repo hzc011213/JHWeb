d1e1e293510f4d36bf091e726c39a721a195d42d
hzc011213 <hzc011213@gmail.com>
2026-06-24 00:43:56 -0400

    refactor: add avp asset

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -1,32 +1,3 @@
-'use client';
-
-import CardSwap, { Card } from '@/components/react-bits/CardSwap';
-
 export default function DesignPage() {
-  return (
-    <main>
-      <div style={{ height: '600px', position: 'relative' }}>
-        <CardSwap
-          cardDistance={-10}
-          verticalDistance={30}
-          delay={3000}
-          pauseOnHover={false}
-          skewAmount={-10}
-        >
-          <Card>
-            <h3>Card 1</h3>
-            <p>Your content here</p>
-          </Card>
-          <Card>
-            <h3>Card 2</h3>
-            <p>Your content here</p>
-          </Card>
-          <Card>
-            <h3>Card 3</h3>
-            <p>Your content here</p>
-          </Card>
-        </CardSwap>
-      </div>
-    </main>
-  );
+  return <main>Design Page</main>;
 }
